caecd9ae95a0464ac9ea5232da389f7050b678af
Fallrain1763 <liushuchen842@icloud.com>
2023-11-17 20:19:13 -0800

    Set up the files

---
+++ docs/UD/main.js
@@ -0,0 +1,31 @@
+title = "Unstable Direction";
+
+description = `
+[Tap to Start]
+`;
+
+characters = [];
+
+options = {};
+
+/** @type {Vector[]} */
+let pins;
+let nextPinDist;
+
+function update() {
+  if (!ticks) {
+    pins = [vec(50, 5)];
+    nextPinDist = 5;
+  }
+  let scr = 0.02;
+  remove(pins, (p) => {
+    p.y += scr;
+    box(p, 3);
+    return p.y > 102;
+  });
+  nextPinDist -= scr;
+  while (nextPinDist < 0) {
+    pins.push(vec(rnd(10, 90), -2 - nextPinDist));
+    nextPinDist += rnd(5, 15);
+  }
+}
