c81a928e728296a461d941d9e2798fb12961a912
Fallrain1763 <liushuchen842@icloud.com>
2023-11-17 20:57:40 -0800

    add player and movement, lose when colliding

--- docs/UD/main.js
+++ docs/UD/main.js
@@ -4,12 +4,23 @@ description = `
 [Tap to Start]
 `;
 
-characters = [];
+characters = [
+  `
+yyy
+yyy
+yyy
+`
+];
 
 options = {};
 
+/** @typedef {{pos: Vector}} Player */
+
 /** @type {Vector[]} */
 let pins;
+/** @type {Player} */
+let player
+
 let nextPinDist;
 let direction = "L";  
 let prevTicks = 0;
@@ -18,8 +29,11 @@ function update() {
   if (!ticks) {
     pins = [vec(50, 5)];
     nextPinDist = 5;
+    player = {pos: vec(50, 95)};
   }
 
+  char("a", player.pos);
+  
   //color("black");
   //text(ticks.toString(), 5, 20);
   
@@ -45,12 +59,23 @@ function update() {
     
     prevTicks = ticks;
   }
-
-   color("black");
+  
+  if(input.isJustPressed)
+  {
+    if(player.pos.x < 90 && direction == "R")
+      player.pos.x += 5;
+    if(player.pos.x > 10 && direction == "L")
+      player.pos.x -= 5;
+  }
+  
+  color("black");
   let scr = difficulty * 0.05;
   remove(pins, (p) => {
     p.y += scr;
-    box(p, 3);
+    if(box(p, 3).isColliding.char.a)
+    {
+      end("GAME OVER");
+    }
     return p.y > 102;
   });
   nextPinDist -= scr;
